89f5e0aa6c65dd1688ee88898114c2d595995f00
Megan Fung <megan.fung@ringcentral.com>
2024-08-02 12:43:51 -0700

    Normalize all files to respect .gitattributes

--- panel.js
+++ panel.js
@@ -1,5 +1,5 @@
 // display summary in popup
-document.addEventListener('DOMContentLoaded', function() {
-    const leadSummary = 'Summary for lead.';
-    document.getElementById('summaryContent').innerText = leadSummary;
+document.addEventListener("DOMContentLoaded", function () {
+  const leadSummary = "Summary for lead.";
+  document.getElementById("summaryContent").innerText = leadSummary;
 });
