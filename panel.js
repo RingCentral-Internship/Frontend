e269271f3429f0051f6a90f9efd826910f8dbe3b
Megan Fung <emailmeganfung@gmail.com>
2024-08-21 13:39:54 -0700

    added general info fields and duplicates

--- panel.js
+++ panel.js
@@ -1,103 +1,116 @@
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Panel.js is loaded and listening for messages");  // wait for script to load 
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Panel.js is loaded and listening for messages"); // wait for script to load
 
-    const loadingElement = document.getElementById("loading");
-    const accordionContainer = document.getElementById("accordionContainer");
-    const chatLog = document.getElementById("chatLog");
-    const chatInpput = document.getElementById("chatInput");
-    const sendButton = document.getElementById("sendButton");
+  const loadingElement = document.getElementById("loading");
+  const accordionContainer = document.getElementById("accordionContainer");
+  const chatLog = document.getElementById("chatLog");
+  const chatInpput = document.getElementById("chatInput");
+  const sendButton = document.getElementById("sendButton");
 
-    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-        if (request.type === "ping") {  // side panel window is ready
-            sendResponse({ status: "ready" });
-        } else if (request.type === "displayLoading"){  // display loading gif-- waiting for query response
-            loadingElement.style.display = "flex";  // render loading icon 
-            accordionContainer.style.display = "none"; // don't show until query is complete 
-        } else if (request.type === "displayLeadData") {  // render data from query 
-            console.log("Message received in panel.js:", request);
-            const isLightning = request.isLightningExperience;  // store interface
-            const leadData = request.data;  // data in message
-            console.log("Lead Data: ", leadData);
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.type === "ping") {
+      // side panel window is ready
+      sendResponse({ status: "ready" });
+    } else if (request.type === "displayLoading") {
+      // display loading gif-- waiting for query response
+      loadingElement.style.display = "flex"; // render loading icon
+      accordionContainer.style.display = "none"; // don't show until query is complete
+    } else if (request.type === "displayLeadData") {
+      // render data from query
+      console.log("Message received in panel.js:", request);
+      const isLightning = request.isLightningExperience; // store interface
+      const leadData = request.data; // data in message
+      console.log("Lead Data: ", leadData);
 
-            loadingElement.style.display = "none";  // stop rendering loading icon 
-            accordionContainer.style.display = "block"; // render accordion toggles
+      loadingElement.style.display = "none"; // stop rendering loading icon
+      accordionContainer.style.display = "block"; // render accordion toggles
 
-            /* sections for html page */
-            const sections = {
-                leadInfo: ["Name", "Company", "Email", "Phone", "Status", "Title"],
-                productInterest: ["Product Interest"],
-                whereWhy: ["Where and Why"],
-                historicalRelationship: ["Historical Relationship"],
-                duplicateLeads: ["Duplicate Leads"],
-                salesHook: ["Sales Enablement Hook"]
-            };
+      /* sections for html page */
+      const sections = {
+        leadInfo: ["Name", "Company", "Email", "Phone", "Status", "Title", "Segment Name", "SM Employees"],
+        productInterest: ["Product Interest"],
+        whereWhy: ["Where and Why"],
+        historicalRelationship: ["Historical Relationship"],
+        duplicates: ["Duplicate Leads", "Duplicate Opportunities"],
+        salesHook: ["Sales Enablement Hook"],
+      };
 
-            for (const section in sections) {  // traverse each section and render data 
-                sections[section].forEach(field => {
-                    const element = document.getElementById(field.replace(/ /g, '_'));
-                    if (element) {
-                        const formattedText = formatText(
-                          leadData[field] || "N/A",
-                          field, 
-                          isLightning
-                        );
+      for (const section in sections) {
+        // traverse each section and render data
+        sections[section].forEach((field) => {
+          const element = document.getElementById(field.replace(/ /g, "_"));
+          if (element) {
+            const formattedText = formatText(
+              leadData[field] || "N/A",
+              field,
+              isLightning
+            );
 
-                        if (field === "Duplicate Leads") {  
-                          if (leadData[field] && leadData[field].length > 0) {
-                            // duplicates exist
-                            element.innerHTML = formattedText; // Render links
-                            const duplicateToggle =
-                              document.getElementById("duplicateLeads");
-                            if (duplicateToggle) {
-                              duplicateToggle.style.display = "block"; // Show the toggle
-                            }
-                          } else {
-                            // no duplicates exist
-                            const duplicateToggle =
-                              document.getElementById("duplicateLeads");
-                            if (duplicateToggle) {
-                              duplicateToggle.style.display = "none"; // Hide the toggle
-                            }
-                          }
-                        } else {
-                          element.innerHTML = formattedText; // render AI generated summaries
-                        }
+            if (section === duplicates) {
+              if (leadData["Duplicate Leads"] && leadData["Duplicate Opportunities"]) {
+                if (leadData["Duplicate Leads"].length + leadData["Duplicate Opportunities"].length > 0) {
+                    // duplicates exist
+                    element.innerHTML = formattedText; // Render links
+                    const duplicateToggle =
+                      document.getElementById("duplicates");
+                    if (duplicateToggle) {
+                      duplicateToggle.style.display = "block"; // Show the toggle
                     }
-                });
-            }
-        }
-    });
-
-    /* logic for accordion toggle visibility */
-    var toggles = document.getElementsByClassName("accordion-toggle");
-    for (var i = 0; i < toggles.length; i++) {
-        toggles[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-                content.style.display = "none";
+                } else {
+                    // no duplicates exist
+                    const duplicateToggle = document.getElementById("duplicates");
+                    if (duplicateToggle) {
+                    duplicateToggle.style.display = "none"; // Hide the toggle
+                    }
+                }
+              } 
             } else {
-                content.style.display = "block";
+              element.innerHTML = formattedText; // render AI generated summaries
             }
+          }
         });
+      }
     }
+  });
 
+  /* logic for accordion toggle visibility */
+  var toggles = document.getElementsByClassName("accordion-toggle");
+  for (var i = 0; i < toggles.length; i++) {
+    toggles[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
 });
 
 /* format json response for html */
 function formatText(text, fieldName, isLightning) {
-  if (fieldName === "Duplicate Leads") {
-    // formatting into SFDC links for lead duplicates
+  if (
+    fieldName === "Duplicate Leads" ||
+    fieldName === "Duplicate Opportunities"
+  ) {
     if (!Array.isArray(text) || text.length === 0) return ""; // return empty if no duplicates
 
-    const leadLinks = text.map((id) => {
+    let dupType;
+    if (fieldName === "Duplicate Leads") {  // for lead URL construction 
+        dupType = "Lead"
+    } else {  // for opportunity URL construction
+        dupType = "Opportunity"
+    }
+
+    const links = text.map((id) => {
       // construct SFDC links for lead profiles
       let salesforceURL = isLightning
-        ? `https://rc.lightning.force.com/lightning/r/Lead/${id}/view`
+        ? `https://rc.lightning.force.com/lightning/r/${dupType}/${id}/view`
         : `https://rc.my.salesforce.com/${id}`;
       return `<a href="${salesforceURL}" target="_blank">${id}</a>`; // convert into clickable
     });
-    return leadLinks.join("<br>");
+    return links.join("<br>");
   } else {
     // formatting AI generated summaries
     // Convert **text** to <strong>text</strong>
@@ -110,4 +123,3 @@ function formatText(text, fieldName, isLightning) {
     return text;
   }
 }
-
